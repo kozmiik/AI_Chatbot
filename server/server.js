b57e1792de2e5e9c124d015f3f5ad83b0e30bee5
Caleb <sxchetti@udel.edu>
2025-10-23 01:59:21 -0400

    checkpoint, reset here if lost

--- server/server.js
+++ server/server.js
@@ -3,3 +3,5 @@ import app from "./app.js";
 // Server (local dev only)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+//checkpoint commit reset here if lost
